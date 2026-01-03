90eefb52da8f9acd826849e3df9586f4c4bbd815
cprieto <cprieto@users.noreply.github.com>
2026-01-03 11:57:49 +0000

    deploy: t-redactyl/new-blog@cabed48bffacf79fe72428453f761af87d012b8e

---
+++ theme.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("open-menu"),d=document.getElementById("close-menu"),s=document.getElementById("mobile-menu");e.addEventListener("click",()=>{s.classList.remove("hidden"),e.classList.add("hidden"),d.classList.remove("hidden")}),d.addEventListener("click",()=>{s.classList.add("hidden"),d.classList.add("hidden"),e.classList.remove("hidden")});
+//# sourceMappingURL=theme.js.map
